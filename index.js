91950c68de053df50ab7fb5591eefa04be586ce4
micha01-bit <ebespalov05@gmail.com>
2025-09-28 01:40:59 +0700

    api-1

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
+import { fetchComments } from './modules/api.js' 
+import { updateComments } from './modules/Comments.js' 
 import { renderComments } from './modules/renderComments.js'
-import { initAddCommentListener } from './modules/initListeners.js'
+import { initAddCommentListener } from './modules/initListeners.js' 
+ 
+fetchComments().then(data => {
+  updateComments(data) 
+  renderComments()
+})
 
-renderComments()
+
 initAddCommentListener()
