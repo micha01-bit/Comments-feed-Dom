b1398289526f4e28fb5bcc617e0a883d5faf1196
micha01-bit <ebespalov05@gmail.com>
2025-10-01 00:03:13 +0700

    api-2

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ import { updateComments } from './modules/Comments.js'
 import { renderComments } from './modules/renderComments.js'
 import { initAddCommentListener } from './modules/initListeners.js' 
  
+document.querySelector('.comments').innerHTML = 'Пожалуйста подождите, загружаю комментарии...'
+ 
+
 fetchComments().then(data => {
   updateComments(data) 
   renderComments()
--- modules/initListeners.js
+++ modules/initListeners.js
@@ -1,28 +1,64 @@
 import { comments, updateComments } from './Comments.js'
 import { sanitizeHtml } from './sanitizeHtml.js'
 import { renderComments } from './renderComments.js'
-import { postComment } from './api.js'
-
+import { postComment } from './api.js' 
+ 
+function delay(interval = 300) {
+   return new Promise((resolve) => {
+      setTimeout(() => {
+         resolve();
+      }, interval);
+   });
+}
+ 
 export const initLikeListeners = () => {
-    const likeButtons = document.querySelectorAll('.like-button')
+    const likeButtons = document.querySelectorAll('.like-button');
 
     for (const likeButton of likeButtons) {
-        likeButton.addEventListener('click', (event) => {
-            event.stopPropagation()
+        likeButton.addEventListener('click', async (event) => {
+            event.stopPropagation();
 
-            const index = likeButton.dataset.index
-            const comment = comments[index]
+            const index = likeButton.dataset.index;
+            const comment = comments[index];
 
+            // автовизуализация анимации
+            likeButton.classList.add('-loading-like');
+
+            await delay(500);  // задержка в 500мс
+
+            // убираем анимацию
+            likeButton.classList.remove('-loading-like');
+
+            // обновляем лайк
             comment.likes = comment.isliked
                 ? comment.likes - 1
-                : comment.likes + 1
+                : comment.likes + 1;
+            comment.isliked = !comment.isliked;
 
-            comment.isliked = !comment.isliked
-
-            renderComments()
-        })
+            renderComments();
+        });
     }
-}
+};
+// export const initLikeListeners = () => {
+//     const likeButtons = document.querySelectorAll('.like-button')
+
+//     for (const likeButton of likeButtons) {
+//         likeButton.addEventListener('click', (event) => {
+//             event.stopPropagation()
+
+//             const index = likeButton.dataset.index
+//             const comment = comments[index]
+
+//             comment.likes = comment.isliked
+//                 ? comment.likes - 1
+//                 : comment.likes + 1
+
+//             comment.isliked = !comment.isliked
+
+//             renderComments()
+//         })
+//     }
+// }
 
 export const initReplyListeners = () => {
     const text = document.getElementById('text-input')
@@ -40,18 +76,21 @@ export const initAddCommentListener = () => {
     const name = document.getElementById('name-input')
     const text = document.getElementById('text-input')
 
-    const addButton = document.querySelector('.add-form-button') 
+    const addButton = document.querySelector('.add-form-button')
 
-        addButton.addEventListener('click', function () {
-         if ( name.value.trim() === '' || text.value.trim() === '') {
-             alert('Пожалуйста, заполните форму!')
-             return
-         }
-    // addButton.addEventListener('click', () => {
-    //     if (!name.value || !text.value) {
-    //         console.error('заполните форму')
-    //         return
-    //     }
+    addButton.addEventListener('click', function () {
+        if (name.value.trim() === '' || text.value.trim() === '') {
+            alert('Пожалуйста, заполните форму!')
+            return
+        }
+
+        document.querySelector('.form-loading').style.display = 'block'
+        document.querySelector('.add-form').style.display = 'none'
+        // addButton.addEventListener('click', () => {
+        //     if (!name.value || !text.value) {
+        //         console.error('заполните форму')
+        //         return
+        //     }
 
         // const newComment = {
         //     name: sanitizeHtml(name.value.trim()),
@@ -59,22 +98,23 @@ export const initAddCommentListener = () => {
         //     text: sanitizeHtml(text.value.trim()),
         //     likes: 0,
         //     isLaked: false,
-        // } 
-         
-        postComment(sanitizeHtml(text.value.trim()), sanitizeHtml(name.value.trim())).then 
-        (
-            (data) => {  
-               updateComments(data)
-               renderComments()
-               name.value = ''
-               text.value = ''
-            },
-        )
+        // }
+
+        postComment(
+            sanitizeHtml(text.value.trim()),
+            sanitizeHtml(name.value.trim()),
+        ).then((data) => { 
+             document.querySelector('.form-loading').style.display = 'none'
+             document.querySelector('.add-form').style.display = 'flex' 
+
+            updateComments(data)
+            renderComments()
+            name.value = ''
+            text.value = ''
+        })
 
         // comments.push(newComment)
 
         // renderComments()
-
-       
     })
 }
